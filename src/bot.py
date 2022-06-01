14bbf0c300e9e4a70853220804cb4fa16761604c
Samox <simon.baczynski@gmail.com>
2022-06-01 17:29:51 +0200

    Catch unit in center, start/stop rec trigger

--- src/bot.py
+++ src/bot.py
@@ -9,59 +9,71 @@ class ObserverBot(ObserverAI):
     Check sc2/observer_ai.py for more available functions
     """
 
+    units_recorded = []
+
     async def on_start(self):
         print("Replay on_start() was called")
         # Initializing window as not found, no window handle exists:
         self.found_window = False
         self.recording_started = False
+        self.unit_alive_center = False
 
         # Get map center coordinates used to find units:
-        self.center_x = self.game_info.map_center.x
-        self.center_y = self.game_info.map_center.y
-
-        self.center_precision = 5
+        self.distance_from_center = 10
+        self.center_map = self.game_info.map_center
 
         # Center camera:
-        # self.client.obs_move_camera(self.game_info.map_center)
+        self.client.obs_move_camera(self.game_info.map_center)
 
         # Attempting to find a window that contains a specific substring:
         self.found_window, self.window = find_window(title="StarCraft II")
+        print(f"Window SC2 found? = {self.found_window}")
+
 
     async def on_step(self, iteration: int):
+        # print(iteration)
+
         # If StarCrarft II window was still not found - look for it!
         if not self.found_window:
             self.found_window, self.window = find_window(title="StarCraft II")
-        # else:
-        #     print("Window found")
+        else:
+            # Find Units close to center 
+            units_close_to_center = self.all_units.closer_than(self.distance_from_center, self.center_map)
+            # print(units_close_to_center)
 
-        #     # Otherwise check if unit is in the center and if the recording was started
-        #     units = self.game_data.units
-        #     center_unit_id = None
-        #     for unit_id, unit_type_data in units.items():
-        #         unit_position = unit_type_data.position
-        #         # Checking if the unit is roughly in the center:
-        #         if (
-        #             unit_position.x >= self.center_x - self.center_precision
-        #             or unit_position.x <= self.center_x + self.center_precision
-        #         ) and (
-        #             unit_position.y >= self.center_y - self.center_precision
-        #             or unit_position.y <= +self.center_precision
-        #         ):
-        #             center_unit_id = unit_id
+            # Start recording -> Check if unit close to center exists and was not recorded
+            if(units_close_to_center.exists and self.recording_started == False and units_close_to_center not in self.units_recorded):
+                print("Unit created close to center...")
+                # print(units_close_to_center)
+                self.unit_alive_center = units_close_to_center[0]
+                print(self.unit_alive_center)
+                print(self.unit_alive_center.position)
+                self.client.obs_move_camera(self.unit_alive_center.position)
 
-        # Center camera on the unit:
-        # self.client.obs_move_camera(units[center_unit_id].position)
-        # print("Centered camera")
+                # Add unit to list of recorded units and start recording
+                self.units_recorded.append(units_close_to_center)
+                self.recording_started = True
+                print("*** START RECORDING ***")
 
-        # # Start recording:
-        # print("Started Recording")
+            # Stop recording -> No units close to center
+            if(self.recording_started == True and units_close_to_center.empty):
+                print("*** END RECORDING ***")
+                self.recording_started = False
+                print(f"\nList with recorded Units: {len(self.units_recorded)} \n")
+                # print(self.units_recorded)
 
-    async def on_unit_created(self, unit):
-        print(f"Unit created: {unit}")
-        print(f"Unit position: {unit.position}")
-        print("*** START RECORDING ***")
-        self.client.obs_move_camera(unit.position)
 
-    async def on_unit_destroyed(self, unit_tag: int):
-        print(f"Unit destroyed: {unit_tag}")
-        print("*** END RECORDING ***")
+    # NOT WORKING #  
+    # async def on_unit_created(self, unit):
+    #     print(f"Unit created: {unit}")
+    #     print(f"Unit position: {unit.position}")
+    #     print("*** START RECORDING ***")
+
+    # Bugs - sometimes did not detect destroyed units
+    # async def on_unit_destroyed(self, unit_tag: int):
+    #     print(f"Unit destroyed: {unit_tag}")
+    #     if(self.recording_started == True and self.unit_alive_center.tag == unit_tag):
+    #         print("*** END RECORDING ***")
+    #         self.recording_started = False
+    #         #print(f"\nList with recorded Units: {len(self.units_recorded)}")
+    #         #print(self.units_recorded)
--- src/main.py
+++ src/main.py
@@ -10,12 +10,3 @@ if __name__ == "__main__":
     replay_path = Path("./src/replays/test_replay_spawner.SC2Replay").resolve().as_posix()
 
     run_replay(ai=observer_bot, replay_path=replay_path)
-
-
-# client_bot = Client()
-# client_bot.obs_move_camera((76.0, 80.0))
-
-### Notes:
-
-# Center of the map = 76.0 & 80.0
-# Client -> obs_move_camera(Point2(76.0, 80.0))
