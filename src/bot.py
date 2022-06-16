5529268f1d7ec93e73b154eb240c4263f9ebfe97
Andrzej Białecki <kaszanas@interia.pl>
2022-06-16 15:59:42 +0200

    Code cleanup, deleted comments and deprecated

--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,12 @@
-import asyncio
 import os
-import subprocess
 import sys
 from sc2.observer_ai import ObserverAI
 
 from utils import find_window
-import pyautogui
-import keyboard
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
-from src.ffmpeg_utils import record_window
+from src.ffmpeg_utils import rec_func
 
 
 class ObserverBot(ObserverAI):
@@ -19,64 +15,11 @@ class ObserverBot(ObserverAI):
     Check sc2/observer_ai.py for more available functions
     """
 
-    units_recorded = []
-
-    async def rec_func(self):
-        # print("Rec_Func")
-
-        # TODO: Improved camera control, now when zooming in on a unit it moves out of the camera area
-        # TODO: get rid of: 1) health bar, 2) HUD
-
-        # Wait for the user to confirm that the unit is selected:
-        while True:
-            if keyboard.is_pressed("h"):
-                break
-
-        # Center on the unit:
-        pyautogui.keyDown("ctrl")
-        pyautogui.press("f")
-        pyautogui.keyUp("ctrl")
-
-        # print(f"Unit on screen? = {self.unit_alive_center.is_on_screen}")
-
-        # Rotate camera counterclockwise by 45 degrees:
-        with pyautogui.hold(["insert", "ctrl", "f"]):
-            await asyncio.sleep(2)
-        # Wait for the camera to move to the right spot
-
-        # Recording window starts here:
-        record_window(
-            name="StarCraft II",
-            output_path="".join(["./rec/", self.unit_alive_center.name, "/"]),
-            filename="".join([self.unit_alive_center.name, "_video", ".mkv"]),
-            video_suffix="",
-            video_prefix="",
-            resolution="",
-        )
-
-        # Rotate camera clockwise by 90 degrees:
-        with pyautogui.hold(["delete", "ctrl", "f"]):
-            await asyncio.sleep(1)
-        # await hold_key(key="delete", hold_time=1)
-
-        # Zoom to get another angle on unit
-        with pyautogui.hold(["insert", "ctrl", "f"]):
-            pyautogui.press("end")
-            await asyncio.sleep(1)
-
-        # Rotate camera counterclockwise by 90 degrees:
-        with pyautogui.hold(["delete", "ctrl", "f"]):
-            await asyncio.sleep(1)
-
-        pyautogui.press("end")
-        await asyncio.sleep(2)
-
-        # Kill FFMPEG process --> recordings in 'rec\{unit.name}' folder (.mkv)
-        print("Rec_OFF")
-        subprocess.run(f"""taskkill /im ffmpeg.exe /t /f""")
-
     async def on_start(self):
         print("Replay on_start() was called")
+
+        self.units_recorded = []
+
         # Initializing window as not found, no window handle exists:
         self.found_window = False
         self.recording_started = False
@@ -86,7 +29,7 @@ class ObserverBot(ObserverAI):
         self.distance_from_center = 10
         self.center_map = self.game_info.map_center
 
-        # Center camera --> not working:
+        # TODO: API needs fixing - Center camera --> not working:
         # self.client.obs_move_camera(self.game_info.map_center)
 
         # Attempting to find a window that contains a specific substring:
@@ -94,7 +37,6 @@ class ObserverBot(ObserverAI):
         print(f"Window SC2 found? = {self.found_window}")
 
     async def on_step(self, iteration: int):
-        # print(iteration)
 
         # If StarCrarft II window was still not found - look for it!
         if not self.found_window:
@@ -103,13 +45,12 @@ class ObserverBot(ObserverAI):
             # Center camera:
             self.client.obs_move_camera(self.game_info.map_center)
 
-            # Find Units close to center
+            # Find Units close to center:
             units_close_to_center = self.all_units.closer_than(
                 self.distance_from_center, self.center_map
             )
-            # print(units_close_to_center)
 
-            # Start recording -> Check if unit close to center exists and was not recorded
+            # Start recording -> Check if unit close to center exists and was not recorded:
             if (
                 units_close_to_center.exists
                 and not self.recording_started
@@ -121,16 +62,14 @@ class ObserverBot(ObserverAI):
                 print(self.unit_alive_center)
                 print(self.unit_alive_center.position)
 
-                # os.run("python helper_rotate.py")
                 self.recording_started = True
                 print("*** START RECORDING ***")
-                await self.rec_func()
+                await rec_func()
 
-                # Add unit to list of recorded units and start recording
+                # Add unit to list of recorded units and start recording:
                 self.units_recorded.append(units_close_to_center)
-                # TODO: Press Del button to rotate camera:
 
-            # Stop recording -> No units close to center
+            # Stop recording -> No units close to center:
             if self.recording_started and units_close_to_center.empty:
                 print("*** END RECORDING ***")
                 self.recording_started = False
@@ -148,18 +87,3 @@ class ObserverBot(ObserverAI):
     async def on_unit_took_damage(self, unit, damage):
         # print("unit took damage")
         pass
-
-    # NOT WORKING #
-    # async def on_unit_created(self, unit):
-    #     print(f"Unit created: {unit}")
-    #     print(f"Unit position: {unit.position}")
-    #     print("*** START RECORDING ***")
-
-    # Bugs - sometimes did not detect destroyed units
-    # async def on_unit_destroyed(self, unit_tag: int):
-    #     print(f"Unit destroyed: {unit_tag}")
-    #     if(self.recording_started == True and self.unit_alive_center.tag == unit_tag):
-    #         print("*** END RECORDING ***")
-    #         self.recording_started = False
-    #         #print(f"\nList with recorded Units: {len(self.units_recorded)}")
-    #         #print(self.units_recorded)
--- src/ffmpeg_utils.py
+++ src/ffmpeg_utils.py
@@ -1,7 +1,10 @@
-import os
-import glob
+import asyncio
+
 from pathlib import Path
 import subprocess
+
+import pyautogui
+import keyboard
 
 
 def record_window(
@@ -17,7 +20,7 @@ def record_window(
     video_size = ""
     name = f'"{name}"'
 
-    if (resolution):
+    if resolution:
         print(f"*Rec resolution = {resolution}*")
         video_size = " ".join(["-video_size", resolution])
     else:
@@ -26,7 +29,7 @@ def record_window(
 
     # Create folder for specific recordings
     Path(output_path).mkdir(parents=True, exist_ok=True)
-    
+
     final_path = (
         Path(output_path, video_prefix, filename, video_suffix).resolve().as_posix()
     )
@@ -34,6 +37,60 @@ def record_window(
     subprocess.Popen(
         f"""ffmpeg -y -f dshow -f gdigrab {video_size} -framerate {frame_rate} -i title={name} -vcodec libx264 -preset ultrafast -qp 0 -pix_fmt yuv444p {final_path}"""
     )
+
+    async def rec_func():
+        # print("Rec_Func")
+
+        # TODO: Improved camera control, now when zooming in on a unit it moves out of the camera area
+        # TODO: get rid of: 1) health bar, 2) HUD
+
+        # Wait for the user to confirm that the unit is selected:
+        while True:
+            if keyboard.is_pressed("h"):
+                break
+
+        # Center on the unit:
+        pyautogui.keyDown("ctrl")
+        pyautogui.press("f")
+        pyautogui.keyUp("ctrl")
+
+        # print(f"Unit on screen? = {self.unit_alive_center.is_on_screen}")
+
+        # Rotate camera counterclockwise by 45 degrees:
+        with pyautogui.hold(["insert", "ctrl", "f"]):
+            await asyncio.sleep(2)
+        # Wait for the camera to move to the right spot
+
+        # Recording window starts here:
+        record_window(
+            name="StarCraft II",
+            output_path="".join(["./rec/", self.unit_alive_center.name, "/"]),
+            filename="".join([self.unit_alive_center.name, "_video", ".mkv"]),
+            video_suffix="",
+            video_prefix="",
+            resolution="",
+        )
+
+        # Rotate camera clockwise by 90 degrees:
+        with pyautogui.hold(["delete", "ctrl", "f"]):
+            await asyncio.sleep(1)
+        # await hold_key(key="delete", hold_time=1)
+
+        # Zoom to get another angle on unit
+        with pyautogui.hold(["insert", "ctrl", "f"]):
+            pyautogui.press("end")
+            await asyncio.sleep(1)
+
+        # Rotate camera counterclockwise by 90 degrees:
+        with pyautogui.hold(["delete", "ctrl", "f"]):
+            await asyncio.sleep(1)
+
+        pyautogui.press("end")
+        await asyncio.sleep(2)
+
+        # Kill FFMPEG process --> recordings in 'rec\{unit.name}' folder (.mkv)
+        print("Rec_OFF")
+        subprocess.run(f"""taskkill /im ffmpeg.exe /t /f""")
 
 
 # Notes:
